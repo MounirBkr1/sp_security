761930b395eb972f212106cbe12b0be66fade01b
Mounir Bkr <miirbri1@gmail.com>
2024-07-22 12:06:10 +0100

    first commit

---
+++ src/main/java/com/mnr/sp_sec/SpSecApplication.java
@@ -0,0 +1,13 @@
+package com.mnr.sp_sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpSecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpSecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mnr/sp_sec/SpSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnr.sp_sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpSecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
